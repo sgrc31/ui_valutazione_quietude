0ce420f0d031eca7eefcc377dd3b16ff5f3d80f8
Simone Guercio <s.guercio@gmail.com>
2018-05-27 19:46:10 +0200

    Add actuator choice and intensity settings

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import serial
 import serial.tools.list_ports
 import logging
 import time
+import copy
 from PyQt5.QtWidgets import QApplication, QDialog
 from PyQt5.QtCore import QUrl
 from PyQt5 import uic
@@ -18,6 +19,12 @@ class MyWin(QDialog):
     def __init__(self, parent=None):
         super(MyWin, self).__init__(parent)
         self.percorso = os.path.dirname(os.path.realpath(__file__))
+        self.lista_valori_pwm_originali = [250, 90,
+                                           250, 50,
+                                           250, 60,
+                                           250, 100,
+                                           200, 250]
+        self.lista_valori_pwm = copy.deepcopy(self.lista_valori_pwm_originali)
         self.initUI()
 
     def initUI(self):
@@ -27,7 +34,6 @@ class MyWin(QDialog):
         #carico e linko bottoni suono
         for i in range(1,7):
             self.bottone = getattr(self, 'snd_btn{0:0>2}'.format(i))
-            logging.info(self.bottone)
             self.bottone.clicked.connect(self.lancia_suono)
         #carico e linko bottoni pattern, disabilitati
         for i in range(1,7):
@@ -38,6 +44,8 @@ class MyWin(QDialog):
         for item in serial.tools.list_ports.comports():
             self.comboBox.addItem(item.device)
         self.openSerial_btn.clicked.connect(self.apri_seriale)
+        self.reset_btn.clicked.connect(self.resetta_valori_pattern)
+        self.set_btn.clicked.connect(self.set_valori_pattern)
         #carico media player per i suoni
         self.player = QMediaPlayer()
 
@@ -56,11 +64,11 @@ class MyWin(QDialog):
                         'elettrodomestico': '5',
                         'clacson': '6'
                         }
-        self.tipo_pattern = str.encode(associazioni_pattern[self.sender().text().lower()])
-        logging.info(self.tipo_pattern)
-        self.ser.write(self.tipo_pattern)
+        self.tipo_pattern = associazioni_pattern[self.sender().text().lower()]
+        self.scrivi_seriale(self.tipo_pattern, self.lista_valori_pwm)
         if self.checkBox.checkState() == 2:
             self.tipo_suono2 = self.comboSuoni.currentText().lower()
+            time.sleep(0.9)
             self.lancia_suono2(self.tipo_suono2)
 
     def lancia_suono(self):
@@ -73,7 +81,43 @@ class MyWin(QDialog):
         self.suono = QMediaContent(QUrl.fromLocalFile(os.path.join(self.percorso, 'sounds', '{}{}'.format(nomesuono, '.mp3'))))
         self.player.setMedia(self.suono)
         self.player.play()
-        
+
+    def resetta_valori_pattern(self):
+        #self.lista_valori_pwm = self.lista_valori_pwm_originali
+        self.slider_alte.setValue(self.lista_valori_pwm_originali[0])
+        self.slider_basse.setValue(self.lista_valori_pwm_originali[1])
+        self.slider_alte_3.setValue(self.lista_valori_pwm_originali[2])
+        self.slider_basse_3.setValue(self.lista_valori_pwm_originali[3])
+        self.slider_alte_2.setValue(self.lista_valori_pwm_originali[4])
+        self.slider_basse_2.setValue(self.lista_valori_pwm_originali[5])
+        self.slider_alte_4.setValue(self.lista_valori_pwm_originali[6])
+        self.slider_basse_4.setValue(self.lista_valori_pwm_originali[7])
+        self.slider_alte_6.setValue(self.lista_valori_pwm_originali[8])
+        self.slider_alte_5.setValue(self.lista_valori_pwm_originali[9])
+        self.set_valori_pattern()
+
+    def set_valori_pattern(self):
+        self.lista_valori_pwm[0] = self.slider_alte.sliderPosition()
+        self.lista_valori_pwm[1] = self.slider_basse.sliderPosition()
+        self.lista_valori_pwm[2] = self.slider_alte_3.sliderPosition()
+        self.lista_valori_pwm[3] = self.slider_basse_3.sliderPosition()
+        self.lista_valori_pwm[4] = self.slider_alte_2.sliderPosition()
+        self.lista_valori_pwm[5] = self.slider_basse_2.sliderPosition()
+        self.lista_valori_pwm[6] = self.slider_alte_4.sliderPosition()
+        self.lista_valori_pwm[7] = self.slider_basse_4.sliderPosition()
+        self.lista_valori_pwm[8] = self.slider_alte_6.sliderPosition()
+        self.lista_valori_pwm[9] = self.slider_alte_5.sliderPosition()
+        logging.info(self.lista_valori_pwm)
+        self.scrivi_seriale(0, self.lista_valori_pwm)
+
+    def scrivi_seriale(self, pattern, valori_pwm):
+        self.tipo_attuatore = 0 if self.radioBtn_led.isChecked() else 1
+        self.payload = str.encode('{} {} {} {} {} {} {} {} {} {} {} {}'.format(self.tipo_attuatore,
+                                                                                     pattern,
+                                                                                     *valori_pwm))
+        self.ser.write(self.payload)
+        logging.info(self.payload)
+
 
 ################
 ##  Start it  ##
